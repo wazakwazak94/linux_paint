5029111868a55a41b504d98846d310ffb2f5d775
Beomseok Moon <mbsk0817@gmail.com>
2018-11-20 12:51:11 +0900

    default window size

--- app/main.c
+++ app/main.c
@@ -1,11 +1,11 @@
-#include "include/util.h"
+#include "util.h"
 
 int main (int argc, char** argv)
 {
 	GtkApplication *app;
 	int status;
 
-	app = gtk_application_new ("linux_paint", G_APPLICATION_FLAGS_NONE);
+	app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
 	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
 	status = g_application_run (G_APPLICATION (app), argc, argv);
 	g_object_unref(app);
--- include/util.h
+++ include/util.h
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <gtk/gtk.h>
 
 static void 
@@ -8,7 +7,7 @@ activate (GtkApplication* app,
     GtkWidget *window;
 
     window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Linus Paint");
+    gtk_window_set_title (GTK_WINDOW (window), "Linux Paint");
     gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
-    gtk_window_show_all (window);
+    gtk_widget_show_all (window);
 }
