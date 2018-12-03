31d6b203b317ba013e3deae745eb4e6d93ab21a9
Beomseok Moon <mbsk0817@gmail.com>
2018-12-03 20:30:04 +0900

    update erase.h

--- include/erase.h
+++ include/erase.h
@@ -5,11 +5,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+GtkWidget *window_erase;
+GtkWidget *entry_erase;
+GtkWidget *scale_erase;
+
 void
 erase_util (GtkWidget *widget,
                 gpointer   data)
 {
-    
+    GtkWidget *button_ok;
+    GtkWidget *button_cancel;
+
+    GtkWidget *fixed;
+
+    window_erase = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title (GTK_WINDOW(window_erase), "Eraser");
+    gtk_window_set_default_size (GTK_WINDOW(window_erase),400,80);
+    gtk_container_set_border_width (GTK_CONTAINER(window_erase),8);
+
+    gtk_widget_show_all(window_erase);
 }
 
 #endif
